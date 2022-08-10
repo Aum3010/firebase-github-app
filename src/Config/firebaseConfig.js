733725ca4b466a739ded57218f4108eb24360aa6
Aum Pandya <73585014+Aum3010@users.noreply.github.com>
2022-08-10 21:36:44 +0530

    firebase-github-app

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyB_b537BHy_dTAcvN4tdTiArVfTOshGuFM",
+    authDomain: "mygitapp-5ac31.firebaseapp.com",
+    projectId: "mygitapp-5ac31",
+    storageBucket: "mygitapp-5ac31.appspot.com",
+    messagingSenderId: "340692306901",
+    appId: "1:340692306901:web:410236b5e14dad44308b5c",
+    measurementId: "G-679RS07RGC"
+  };
+
